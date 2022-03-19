5fd073cd6f9b1bd6f785acf8f47eed9c8bcff53a
sultanoss <radwansultan@hotmail.dem>
2022-03-20 00:42:11 +0100

    game first step

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StartScreenComponent } from './start-screen/start-screen.component';
+import { GameComponent } from './game/game.component';
+import { PlayerComponent } from './player/player.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StartScreenComponent,
+    GameComponent,
+    PlayerComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/game/game.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit } from '@angular/core';
+import { Game } from 'src/models/game';
+
+@Component({
+  selector: 'app-game',
+  templateUrl: './game.component.html',
+  styleUrls: ['./game.component.scss']
+})
+export class GameComponent implements OnInit {
+  pickCardAnimation = false;
+  currentCard: string = '';
+  game: Game;
+  constructor() { }
+
+  ngOnInit(): void {
+    this.newGame();
+  }
+
+  newGame() {
+
+    this.game = new Game();
+    console.log(this.game);
+
+  }
+
+  takeCard() {
+    if (!this.pickCardAnimation) {
+
+      this.currentCard = this.game.stack.pop();
+      this.pickCardAnimation = true;
+      console.log(this.game);
+      setTimeout(() => {
+        this.game.playedCards.push(this.currentCard);
+        this.pickCardAnimation = false;
+      }, 1000);
+
+    }
+
+  }
+
+}

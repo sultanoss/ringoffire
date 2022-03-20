0c9214459ab179b6ccb4582772ae455632c60af9
sultanoss <radwansultan@hotmail.dem>
2022-03-20 23:07:04 +0100

    Added game-rules

--- src/app/game-info/game-info.component.ts
+++ src/app/game-info/game-info.component.ts
@@ -1,15 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnChanges, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-game-info',
   templateUrl: './game-info.component.html',
-  styleUrls: ['./game-info.component.scss']
+  styleUrls: ['./game-info.component.scss'],
 })
-export class GameInfoComponent implements OnInit {
+export class GameInfoComponent implements OnInit, OnChanges {
+  cardAction = [
+    { tittle: 'drink', description: 'drink' },
+    { tittle: 'drink', description: 'dance' },
+    { tittle: 'drink', description: 'jump' },
+    { tittle: 'drink', description: 'eat1' },
+    { tittle: 'drink', description: 'eat2' },
+    { tittle: 'drink', description: 'eat3' },
+    { tittle: 'drink', description: 'eat4' },
+    { tittle: 'drink', description: 'eat5' },
+    { tittle: 'drink', description: 'eat6' },
+    { tittle: 'drink', description: 'eat7' },
+    { tittle: 'drink', description: 'eat8' },
+    { tittle: 'drink', description: 'eat9' },
+    { tittle: 'drink', description: 'eat10' },
+  ];
 
-  constructor() { }
+  title = '';
+  description = '';
+  @Input() card: string;
 
-  ngOnInit(): void {
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  ngOnChanges(): void {
+    if (this.card) {
+      let cardNummber = +this.card.split('_')[1];
+      this.title = this.cardAction[cardNummber - 1].tittle;
+      this.description = this.cardAction[cardNummber - 1].description;
+    }
   }
-
 }
